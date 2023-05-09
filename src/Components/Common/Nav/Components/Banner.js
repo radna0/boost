23cea4b228be25de71b37a6284997313acd664e2
Bach HG <87762857+radna0@users.noreply.github.com>
2023-05-09 11:05:53 +0700

    Adjust file redirect

--- src/Components/Common/Nav/Components/Banner.js
+++ src/Components/Common/Nav/Components/Banner.js
@@ -17,7 +17,7 @@ function Banner() {
     return (
  
         <div className="nav_logo">
-            <Link to="/boost3.0" >
+            <Link to="/boost" >
                 <img src="Assets/Icons/Logo/logo.png" alt="" />
             </Link>
         </div>
