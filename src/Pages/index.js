a2540feaecb79b592bd50b3df3e475856cfbbc04
Bach HG <87762857+radna0@users.noreply.github.com>
2023-05-09 11:26:09 +0700

    Updates

--- src/Pages/index.js
+++
@@ -1,9 +0,0 @@
-import Home from './Home/Home'
-import About from './About/About'
-import Career from './Career/Career'
-import Feature from './Feature/Feature'
-import FAQ from './FAQ/FAQ'
-
-
-
-export { Home , About, Feature , Career , FAQ }
